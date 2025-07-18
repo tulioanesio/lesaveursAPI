6b4449a4a7af1309e167302b5c51cbbfa19e39f5
Tulio <tulioluv@gmail.com>
2025-07-18 15:11:05 -0300

    fix: insert de mesas no bd corrigido

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,40 +1,26 @@
-// prisma/seed.ts
-
 import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
 async function main() {
-  // Array de mesas
-  const mesas = [
-    { numero: 1, capacidade: 2 },
-    { numero: 2, capacidade: 4 },
-    { numero: 3, capacidade: 2 },
-    { numero: 4, capacidade: 6 },
-    { numero: 5, capacidade: 4 },
-    { numero: 6, capacidade: 4 },
-    { numero: 7, capacidade: 8 },
-    { numero: 8, capacidade: 2 },
-    { numero: 9, capacidade: 2 },
-    { numero: 10, capacidade: 4 },
-    { numero: 11, capacidade: 6 },
-  ];
-
-  // Criação em massa
-  for (const mesa of mesas) {
-    await prisma.mesa.upsert({
-      where: { numero: mesa.numero },
-      update: {},
-      create: {
-        ...mesa,
-        disponivel: true,
-      },
-    });
-  }
-
-  console.log('Mesas inseridas com sucesso!');
+  const createMany = await prisma.mesa.createMany({
+    data: [
+      { numero: 1, capacidade: 2 },
+      { numero: 2, capacidade: 4 },
+      { numero: 3, capacidade: 2 },
+      { numero: 4, capacidade: 6 },
+      { numero: 5, capacidade: 4 },
+      { numero: 6, capacidade: 4 },
+      { numero: 7, capacidade: 8 },
+      { numero: 8, capacidade: 2 },
+      { numero: 9, capacidade: 2 },
+      { numero: 10, capacidade: 4 },
+      { numero: 11, capacidade: 6 },
+    ],
+    skipDuplicates: true,
+  });
+  console.log(`${createMany.count} mesas inseridas com sucesso!`);
 }
-
 main()
   .catch((e) => {
     console.error(e);
